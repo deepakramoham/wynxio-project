1b8b3f45006e6fb321cf8f2b422b19d91b3fb30f
Deepak Janardhanan <deepakramoham@gmail.com>
2025-11-19 19:08:11 +0530

    moved code from html file to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,70 @@
+import { useState } from "react";
+
+function ListInput({ students, setStudents }) {
+  const [inputValue, setInputValue] = useState("");
+  console.log("list input function is running");
+
+  console.log(inputValue);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddButton = () => {
+    setStudents([...students, { id: crypto.randomUUID(), name: inputValue }]);
+
+    console.log(students);
+    setInputValue("");
+  };
+
+  return (
+    <>
+      <input
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your name"
+      />
+      <button onClick={handleAddButton}>Add</button>
+    </>
+  );
+}
+
+function ListItem({ id, name }) {
+  console.log("list item running");
+
+  return (
+    <div className="list-item">
+      <div>
+        {name} <button>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+function ListData({ students }) {
+  console.log("list data running");
+  return students?.map((student) => (
+    <ListItem key={student.id} name={student.name} id={student.id} />
+  ));
+}
+
 function App() {
-  return <>Welcome to react</>;
+  const [students, setStudents] = useState([
+    { id: "dpk", name: "Deepak" },
+    { id: "srl", name: "Siril" },
+    { id: "tmn", name: "Tomin" },
+    { id: "hdr", name: "Hyder" },
+    { id: "ans", name: "Anas" },
+  ]);
+
+  console.log("app is running");
+
+  return (
+    <>
+      <ListInput students={students} setStudents={setStudents} />
+      <ListData students={students} />
+    </>
+  );
 }
 
 export default App;
