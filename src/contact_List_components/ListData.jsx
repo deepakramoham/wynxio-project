84680aab1b48cbe4bfe68f289743f62d6959ceca
Deepak Janardhanan <deepakramoham@gmail.com>
2025-11-21 19:53:29 +0530

    conditional rendering with default operator

--- src/contact_List_components/ListData.jsx
+++ src/contact_List_components/ListData.jsx
@@ -1,28 +1,27 @@
 import ListItem from "./ListItem";
 
 function ListData({ students, handleDeleteButton }) {
- 
-
   let content = null;
 
   if (students?.length > 0) {
     content = students;
-  } else {
-    content = "No Contact Found";
   }
 
   //We cannot use if else inside jsx. In such circumstances use ternary, default or guard operator.However we can use if else out of jsx
 
-  return Array.isArray(content) ? (
-    content?.map((student) => (
-      <ListItem
-        key={student.id}
-        student={student}
-        handleDeleteButton={handleDeleteButton}
-      />
-    ))
-  ) : (
-    <p>{content}</p>
+  return (
+    <>
+      {Array.isArray(content) &&
+        content?.map((student) => (
+          <ListItem
+            key={student.id}
+            student={student}
+            handleDeleteButton={handleDeleteButton}
+          />
+        ))}
+
+      {!content && <p>No contact found</p>}
+    </>
   );
 }
 
